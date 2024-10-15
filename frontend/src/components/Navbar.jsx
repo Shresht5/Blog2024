caaa1d1d0aedf6f2d5b3c5e60bd6d0f53d730c27
Shresht5 <shreshtabalapure5@gmail.com>
2024-10-15 19:59:21 +0530

    redux

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 const Navbar = () => {
+    const isLogin=useSelector(state=>state.isLogin);
+    console.log(isLogin)
     return (
         <div className='sticky bg-blue-500 p-4 shadow-xl text-white'>
             <div className='max-w-6xl mx-auto flex justify-between items-center '>
@@ -12,11 +15,17 @@ const Navbar = () => {
 
                 </div>
                 <div className='space-x-4 '>
-                    <Link to="/myblog"><button >My Blogs</button></Link>
-                    <Link to="/blogs"><button >Blogs</button></Link>
-                    <Link to="/login"><button >Login</button></Link>
+                    {isLogin && (<>
+                        <Link to="/myblog"><button >My Blogs</button></Link>
+                        <Link to="/blogs"><button >Blogs</button></Link>
+                    </>)}
+                    {!isLogin && (<>
+                        <Link to="/login"><button >Login</button></Link>
                     <Link to="/signup"><button>Sign Up</button></Link>
+                    </>)}
+                    {isLogin && (<>
                     <Link to="/login"><button>Log Out</button></Link>
+                    </>)}
                 </div>
             </div>
         </div>
